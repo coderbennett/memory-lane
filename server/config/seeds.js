768363e45483ee1771bbcf3194035f5097febc43
coderbennett <jbennett@csumb.edu>
2022-06-07 11:06:21 -0700

    add seeds to config, add seed command in package.json

---
+++ server/config/seeds.js
@@ -0,0 +1,38 @@
+const db = require('./connection');
+const { User, Timeline } = require('../models');
+
+db.once('open', async () => {
+    await Timeline.deleteMany();
+
+    const timelines = await Timeline.insertMany([
+        { title: 'Star Wars Timeline', description: 'A timeline of when all the Star Wars shows and movies occurred', author: 'yodafather'},
+        { title: 'Polar Ice Caps', description: 'A timeline of the polar ice caps over the past 40 years.', author: 'meltingicequeen'},
+        { title: 'Major US Historical Events', description: 'A timeline of major historical US events', author: 'notaspy'}
+    ]);
+
+    console.log('timelines seeded');
+
+    await User.deleteMany();
+
+    await User.create({
+        username: 'yodafather',
+        email: 'yoda@jedicouncil.com',
+        password: 'sizemattersnot'
+    });
+
+    await User.create({
+        username: 'meltingicequeen',
+        email: 'elsa@cold.com',
+        password: 'letitGO'
+    });
+
+    await User.create({
+        username: 'notaspy',
+        email: 'nsa@gov.com',
+        password: 'oops'
+    });
+
+    console.log('users seeded');
+
+    process.exit();
+});
