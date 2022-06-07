df2201bbcdf145e26a48fab530fa004f463ea8f4
coderbennett <jbennett@csumb.edu>
2022-06-06 20:45:50 -0700

    add timeline typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,24 @@ const typeDefs = gql`
         password: String
         timelines: [Timeline]
     }
+
+    type Timeline {
+        _id: ID
+        title: String
+        description: String
+        author: String
+        moments: [Moment]
+    }
+
+    type Moment {
+        _id: ID
+        title: String
+        description: String
+        imageLink: String
+        year: Int
+        month: Int
+        day: Int
+    }
 `;
 
 module.exports = typeDefs;
