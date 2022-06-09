437fff928eb6d452d2eeff5ade710441b7ee9f7d
Maung Htike <maunghtike1999@gmail.com>
2022-06-09 10:57:55 -0700

    add link to home button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <button className="btn btn-ghost normal-case text-xl">daisyUI</button>
+                <a href="/" className="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
