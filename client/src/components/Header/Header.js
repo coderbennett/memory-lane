cd087729476b51583e0704072781feb00c8a1de9
Maung Htike <maunghtike1999@gmail.com>
2022-06-09 10:54:32 -0700

    fix error code on console

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
                     <li><a>Item 1</a></li>
-                    <li tabindex="0">
+                    <li tabIndex="0">
                         <a>
                             Parent
                             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
