48107f0913c8dedceecf5884addada3d3024509f
Maung Htike <maunghtike1999@gmail.com>
2022-06-09 10:49:58 -0700

    change version from react 18 to 17, update gql

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
-//import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Header from '../src/components/Header/Header';
 
-/*
+
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
-*/
+
 function App() {
   return (
-    // <ApolloProvider client={client}>
-    <Router>
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />} />
-      </Routes>
-    </Router>
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
