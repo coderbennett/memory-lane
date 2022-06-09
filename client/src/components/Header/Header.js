c77a1ae7af0f5b132e5e0b882e9491ffed239543
Maung Htike <maunghtike1999@gmail.com>
2022-06-09 10:56:49 -0700

    changed eslint formatting issue

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <button className="btn btn-ghost normal-case text-xl">daisyUI</button>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
