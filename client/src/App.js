2815bfa87191a39e25eded5f136e11763147e380
Maung Htike <maunghtike1999@gmail.com>
2022-06-08 20:49:38 -0700

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import * as React from "react";
+import React from "react";
+//import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Header from '../src/components/Header/Header';
 
+/*
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+*/
 function App() {
   return (
-
-    "hello"
-
+    // <ApolloProvider client={client}>
+    <Router>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />} />
+      </Routes>
+    </Router>
+    // </ApolloProvider>
   );
 }
 
