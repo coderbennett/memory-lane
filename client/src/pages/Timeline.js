4c8d53cd91b87829ecaa30a58f19e2e15dcee222
coderbennett <jbennett@csumb.edu>
2022-06-09 13:24:19 -0700

    add updated formatting for timeline page

--- client/src/pages/Timeline.js
+++ client/src/pages/Timeline.js
@@ -3,23 +3,21 @@ import { useMutation, useQuery } from '@apollo/client';
 
 import { Navigate, useParams } from 'react-router-dom';
 
-import { QUERY_TIMELINE } from '../../utils/queries';
+import { QUERY_TIMELINE } from '../utils/queries';
 //we need to add the edit buttons here
-import Auth from '../../utils/auth';
+import Auth from '../utils/auth';
 
 const Timeline = () => {
     //grab the timeline id from the URL parameters
     const { timelineId } = useParams();
-
     //useQuery to query the timeline at the given ID
-    const { loading, data } = useQuery(
-        QUERY_TIMELINE,
-        {
-            variables: { timelineId }
+    const { loading, data } = useQuery( QUERY_TIMELINE, {
+            variables: { timelineId: timelineId }
         }
     );
 
-    const timeline = data;
+    const timeline = data?.timeline || {};
+    console.log(timeline);
 
     // this function renders the moments
     // takes in a conditional to decide which moments
@@ -38,34 +36,37 @@ const Timeline = () => {
         return (
             <div className="flex flex-row justify-evenly">
                 {isEven ? (
-                    <div className='w-64'></div>
+                    <div className='w-96'></div>
                 ) : (
                 <></>
                 )}
                 {momentArray.map((moment) => (
                 <>
-                    <div key={moment._id} className="w-64 rounded">
-                        <div className="flex flex-row">
-                            <img src={moment.imageLink} alt={moment.title}></img>
-                            <div className="flex flex-col justify-left ml-3">
-                                <h3>{moment.title}</h3>
-                                <p>{moment.description}</p>
-                                <h4>{moment.month} {moment.day} {moment.year}</h4>
+                    <div key={moment._id} class="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src={moment.imageLink} alt={moment.title} /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">{moment.month} {moment.day} {moment.year}</h2>
+                            <p>{moment.description}</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Edit Moment</button>
                             </div>
                         </div>
                     </div>
-                    <div className="w-64"></div>
+                    <div className="w-96"></div>
                 </>
                 ))}
             </div>
         )
+    }
+    if (loading) {
+        return <div>Loading...</div>
     }
 
     return (
         <section id="timeline">
             <h2>{timeline.title}</h2>
             {renderMoments(false)}
-            <hr></hr>
+            <hr className="my-6"></hr>
             {renderMoments(true)}
         </section>
     )
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,21 +1,21 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_TIMELINE = gql`
-    getTimeline($timelineId: ID!) {
+    query getTimeline($timelineId: ID!) {
         timeline(timelineId: $timelineId) {
+          _id
+          title
+          description
+          author
+          moments {
             _id
             title
             description
-            author
-            moments {
-                _id
-                title
-                description
-                imageLink
-                year
-                month
-                day
-            }
+            imageLink
+            year
+            month
+            day
+          }
         }
-    }
+      }
 `
