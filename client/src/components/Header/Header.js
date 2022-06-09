fb9aefb3d5adc51f33d360d0e1b6282ea5b6a3da
Maung Htike <maunghtike1999@gmail.com>
2022-06-09 14:32:28 -0700

    add button on header for login

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -14,28 +14,31 @@ export default function Header() {
                     <li>
                         <label for="my-modal-4" className="btn modal-button bg-primary">Login</label>
 
-
                         <input type="checkbox" id="my-modal-4" className="modal-toggle" />
                         <label for="my-modal-4" className="modal cursor-pointer">
                             <label className="modal-box relative" for="">
-                                <h3 className="text-lg font-bold">Login Here!</h3>
+                                <h3 className="text-lg font-bold">Create Timeline</h3>
 
-                                <div className="form-control">
+                                <div className="form-control ">
+                                    <div className="grid grid-cols-2">
+                                        <label className="my-3  input-group input-group-vertical">
+                                            <span className="py-1">Title</span>
+                                            <input type="text" placeholder="Timeline Title" className="input input-bordered" />
+                                        </label>
+                                    </div>
                                     <label className="my-3 input-group input-group-vertical">
-                                        <span className="py-1">Email</span>
-                                        <input type="text" placeholder="email@domain.com" className="input input-bordered" />
+                                        <span className="py-1">Description</span><textarea class="textarea textarea-bordered" placeholder="Write a little bit about your timeline here"></textarea>
                                     </label>
-                                    <label className="my-3 input-group input-group-vertical">
-                                        <span className="py-1">Password</span>
-                                        <input type="password" placeholder="password" className="input input-bordered" />
-                                    </label>
-                                    <div className="flex flex-row justify-between">
-                                        <button type="submit" className="btn btn-primary">Login</button>
-                                        <button type="submit" className="btn btn-primary">Signup</button>
+
+                                    <div className="flex flex-col">
+                                        <button type="submit" className="btn btn-primary ">Create Timeline</button>
                                     </div>
                                 </div>
                             </label>
                         </label>
+
+
+
                     </li>
 
 
