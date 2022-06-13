149b4eb82a36432031527621b4c3dd09ad09edd8
Maung Htike <maunghtike1999@gmail.com>
2022-06-12 21:30:47 -0700

    add custom theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,4 +4,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mltheme1: {
+          "primary": "#FFF6E8",
+          "secondary": "#F8EDEB",
+          "accent": "#FF9090",
+          "neutral": "#322100",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        }
+      }
+    ]
+  }
 }
