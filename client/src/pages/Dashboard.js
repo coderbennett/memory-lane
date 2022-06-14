1f83c7d00fce3038b742a85dc7c81fd66b9af93f
coderbennett <jbennett@csumb.edu>
2022-06-13 19:53:28 -0700

    refactor dashboard and createtimeline component, add comments

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Navigate, useParams, Link } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
-
 import { QUERY_USER, QUERY_TIMELINES } from '../utils/queries';
 import { DELETE_TIMELINE } from '../utils/mutations';
-
 import CreateTimeline from "../components/modals/CreateTimeline";
-
 import Auth from '../utils/auth';
 
 const Dashboard = () => {
+    //grab the userId from the url with useParams
     const { userId } = useParams();
 
+    //here we are grabbing the mutation delete timeline to be used shortly
     const [deleteTimeline] = useMutation(DELETE_TIMELINE);
 
+    //we're setting the userId and removing the colon from the url
     let id = userId.substring(1);
 
+    //this function handles the delete button functionality when it is clicked
     const handleDeleteBtn = async (event) => {
+        //we access the name property from the button clicked
+        //with object destructuring
         const { name } = event.target;
+
+        //we try to delete the timeline and refresh the page
+        //if it fails we send an error to the console
         try {
-            const { data } = await deleteTimeline({
+            await deleteTimeline({
                 variables: { timelineId: name }
             });
 
@@ -30,38 +36,40 @@ const Dashboard = () => {
         }
     };
 
+    //we are using usequery here to query the database for a user with the given id
     const { loading, data } = useQuery(
         QUERY_USER, { variables: { userId: id } }
     );
 
+    //here we set the user data to the user variable (if there is no data it is set to an empty object)
     const user = data?.user || {};
 
+    //we also use usequery to query the database for all the timelines with the current user as an author
     const timelineResponse = useQuery(
         QUERY_TIMELINES, { variables: { author: user.username } }
     );
 
+    //we set the timelines variable to the response from the query above
     const timelines = timelineResponse.data?.userTimelines || [];
 
+    //if you're logged in and you accidentally go to the wrong dashboard
+    //somehow we redirect you to the correct dashboard
     if (Auth.loggedIn() && Auth.getUser().data._id !== id) {
         return <Navigate to={"/dashboard/:" + id} />;
     }
 
+    //if you're trying to get here without being logged in we send you to the
+    //homepage
     if (!Auth.loggedIn()) {
         return <Navigate to={"/"} />;
     }
 
+    //while the queries load we post this to the page
     if (loading) {
         return <div>Loading...</div>
     }
 
-    if (!user?.username) {
-        return (
-            <h4>
-                You need to be logged in to see your dashboard page. Use the log in button to log in or sign up.
-            </h4>
-        )
-    }
-
+    //we create two style objects to apply to some of our JSX elements below
     const fontStyle = {
         fontFamily: "Indie Flower",
     }
@@ -74,17 +82,22 @@ const Dashboard = () => {
         textDecoration: "none"
     }
 
+    //this returns our dashboard page with the user's timelines (if they have any)
     return (
         <div className="flex flex-col mt-16">
+
+            {/*we display the user's name at the top and have a createtimeline component, passing the user data as a prop*/}
             <h2 className="mt-16 text-center text-4xl font-bold" style={fontStyle}><u style={underlineStyle}>{user.username}'s Dashboard</u></h2>
             <CreateTimeline user={user} />
+
+            {/*if timelines.length is more than 1 we render 'your timelines' otherwise we let them know they can create a timeline*/}
             {timelines.length ? (
                 <h3 className="my-6 text-center text-2xl font-bold">Your Timelines</h3>
             ) : (
                 <h3 className="my-6 text-center text-2xl font-bold">Create a Timeline to See it Listed Below</h3>
             )}
-            <div className="flex justify-end">
-            </div>
+        
+            {/*if we have timelines then we map across the array and display them in cards top to bottom*/}
             {timelines &&
                 timelines.map((timeline) => (
                     <div key={timeline._id} className="mx-auto border border-zinc-900 my-8 card w-3/4 bg-primary shadow-xl">
